586e210cdd8141657127a146c8b7dab7b940d11a
Vinayak Vohra <vn26vhr@gmail.com>
2024-05-01 21:44:01 +0530

    Time: 333 ms (96.51%), Space: 168 MB (74.4%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,44 +1,22 @@
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> findWinners(vector<vector<int>> &matches)
-    {
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         unordered_map<int, int> won;
         unordered_map<int, int> lost;
         vector<vector<int>> res(2, vector<int>());
 
-        for (auto &match : matches)
-        {
-            // if (won[match[0]] == 0 &&              // first win
-            //     lost.find(match[0]) == lost.end()) // no loses
-            // {
-            //     wonAll.insert(match[0]);
-            // }
-
-            // auto it = won.find(match[1]);
-            // if (it != won.end())
-            //     wonAll.erase(it->first);
-
-            // it = lost.find(match[1]);
-            // if (it == lost.end())
-            //     lost1.insert(match[1]);
-
-            // else if (it->second == 1)
-            //     lost1.erase(it->first);
-
+        for (auto& match : matches) {
             won[match[0]]++;
             lost[match[1]]++;
         }
 
-        for(auto&x: won)
-        {
-            if(lost.find(x.first)==lost.end())
+        for (auto& x : won) {
+            if (lost.find(x.first) == lost.end())
                 res[0].push_back(x.first);
         }
 
-        for(auto&x: lost)
-        {
-            if(x.second == 1)
+        for (auto& x : lost) {
+            if (x.second == 1)
                 res[1].push_back(x.first);
         }
         sort(res[0].begin(), res[0].end());
@@ -47,3 +25,10 @@ public:
         return res;
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
