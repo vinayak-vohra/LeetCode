2bc8828d798926c0f31ec85b306e31630ed5fb88
Vinayak Vohra <vn26vhr@gmail.com>
2024-05-01 21:15:50 +0530

    Time: 7 ms (99.53%), Space: 22.8 MB (86.11%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -5,17 +5,25 @@ public:
         max1 = max2 = INT_MIN;
         min1 = min2 = INT_MAX;
 
-        for(int &n: nums)
-        {
-            if(n > max1)
-            max2 = max1, max1 = n;
-            else if(n > max2) max2 = n;
-            
-            if(n < min1)
-            min2 = min1, min1 = n;
-            else if(n < min2) min2 = n;
+        for (int& n : nums) {
+            if (n > max1)
+                max2 = max1, max1 = n;
+            else if (n > max2)
+                max2 = n;
+
+            if (n < min1)
+                min2 = min1, min1 = n;
+            else if (n < min2)
+                min2 = n;
         }
 
         return max1 * max2 - min1 * min2;
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
