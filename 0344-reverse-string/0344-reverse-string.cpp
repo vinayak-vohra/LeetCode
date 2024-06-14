780b3fdbf99d048139f31c6de8ccfa64c5b87f30
Vinayak Vohra <vn26vhr@gmail.com>
2024-06-14 22:29:51 +0530

    Time: 10 ms (95.05%), Space: 26.9 MB (58.87%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,18 +1,10 @@
-#pragma GCC optimize("03", "unroll-loops")
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        for(int i = 0, j = s.size() - 1; i < j; ++i, --j) {
-            auto temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-        }                    
+        int lt = -1, rt = s.size();
+
+        while(++lt < --rt)
+            std::swap(s[lt], s[rt]);
+        
     }
 };
-
-auto init = [](){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
