58b3c6801f9e6e128b68e3dfd773a1f802bd93da
Vinayak Vohra <vn26vhr@gmail.com>
2024-06-14 22:10:55 +0530

    Time: 69 ms (99.8%), Space: 69 MB (93.9%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int ans = 0;
+        for(int i = 1; i < nums.size(); ++i) {
+            if(nums[i] <= nums[i - 1]) {
+                ans += (nums[i - 1] - nums[i] + 1);
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+auto init = [](){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
